0d467d31d00eb10ffcc7509ea3a626873779b630
usman <ansariusman858@gmail.com>
2026-06-18 13:26:03 +0530

    pandas added

---
+++ f1.py
@@ -0,0 +1,7 @@
+class xyz:
+    def __init__(self,a,b,c):
+        self.a=a
+        self.b=b
+        self.c=c
+    def mno(self):
+        return f"value of a is {self.a} value of b is {self.b} and value of c is {self.c}"
---
+++ main.py
@@ -0,0 +1,3 @@
+from f1 import xyz
+a = xyz(4, 5, 6)
+print(a.mno())
